ea5b38621db34a6536b445e4f85044974a29cb01
Jim Kadin <james.kadin@jax.org>
2018-09-11 14:43:47 -0400

    Fixed basic lib

--- basicLib.py
+++ basicLib.py
@@ -1,3 +1,9 @@
-
-def removeNonAscii(text
-return ''.join([i if ord(i) < 128 else ' ' for i in text])
+#
+# basic utilities that Jim uses
+#
+def removeNonAscii(text):
+    """ return a string with all the non-ascii characters in text replaced
+        by a space.
+        Gets rid of those nasty unicode characters.
+    """
+    return  ''.join([i if ord(i) < 128 else ' ' for i in text])
